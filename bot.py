b4ab6691faf677946874941bcc207df0baefa324
Deshdeepak <rkdeshdeepak1@gmail.com>
2021-06-23 19:09:53 +0530

    Receive from telegram and add config.ini

--- bot.py
+++ bot.py
@@ -1,28 +1,41 @@
+from pyrogram import Client
+from pyrogram import filters
 from bs4 import BeautifulSoup
 import re
 
-file = "video.html"
-with open(file,'r') as f:
-    source = f.read()
+bot = Client('bot', config_file='config.ini')
 
-soup = BeautifulSoup(source,'html.parser')
 
-paras = soup.find_all("p")
+@bot.on_message(filters.command('start'))
+async def start(bot, message):
+    await message.reply("Upload html file")
 
-title = paras[0].string
 
-vids = ''.join([ str(tag) for tag in soup.find_all("p", style="text-align:center;font-size:25px;") ])
-vids_soup = BeautifulSoup(vids, 'html.parser')
-links = [link.extract().text for link in vids_soup.findAll('a')]
-name = re.compile('\d+\..*?(?=<br/>)')
-names = name.findall(vids)
+@bot.on_message(filters.document)
+async def upload(bot, message):
+    if message.document["mime_type"] == "text/html":
+        file = message.document.file_id+'.html'
+        await message.download(file)
 
-vids_dict = dict(zip(names, links))
+        with open('./downloads/'+file,'r') as f:
+            source = f.read()
 
-print(title)
-print()
-print()
-for vid in vids_dict:
-    print('Name: ', vid)
-    print('Link: ', vids_dict[vid])
-    print()
+        soup = BeautifulSoup(source,'html.parser')
+
+        paras = soup.find_all("p")
+
+        title = paras[0].string
+
+        vids = ''.join([ str(tag) for tag in soup.find_all("p", style="text-align:center;font-size:25px;") ])
+        vids_soup = BeautifulSoup(vids, 'html.parser')
+        links = [link.extract().text for link in vids_soup.findAll('a')]
+        name = re.compile('\d+\..*?(?=<br/>)')
+        names = name.findall(vids)
+
+        vids_dict = dict(zip(names, links))
+        await message.reply(title)
+        for vid in vids_dict:
+            video = 'Name: ' + vid + '\n' + 'Link: ' + vids_dict[vid]
+            await message.reply(video)
+
+bot.run()
