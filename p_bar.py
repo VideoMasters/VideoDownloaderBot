817d357f6a6c2545fd13f6697ddfca3cd7f427c2
noahsarkq <86694875+noahsarkq@users.noreply.github.com>
2021-08-01 21:22:42 +0530

    Add files via upload

---
+++ p_bar.py
@@ -0,0 +1,36 @@
+import time
+import math
+import os
+from Easy_F import hrb,hrt
+from pyrogram.errors import FloodWait
+
+class Timer:
+    def __init__(self, time_between=10):
+        self.start_time = time.time()
+        self.time_between = time_between
+
+    def can_send(self):
+        if time.time() > (self.start_time + self.time_between):
+            self.start_time = time.time()
+            return True
+        return False
+
+timer = Timer()
+async def progress_bar(current,total,reply,start):
+      if timer.can_send():
+        now = time.time()
+        diff = now - start
+        if diff < 1:
+            return
+        else:
+            perc = f"{current * 100 / total:.1f}%"
+            elapsed_time = round(diff)
+            speed = current / elapsed_time
+            sp=str(hrb(speed))+"ps"
+            tot=hrb(total)
+            cur=hrb(current)
+            try:
+                await reply.edit(f'{perc}   {sp}    {cur}   {tot}')
+               
+            except FloodWait as e:
+                time.sleep(e.x)
